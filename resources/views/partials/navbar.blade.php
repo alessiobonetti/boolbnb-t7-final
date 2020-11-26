b7e8cad2023d868dd20c4b2de5a37b957f4d213d
Alessio Bonetti <alesine87@gmail.com>
2020-11-26 17:25:45 +0100

    Navbar + Search

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,33 +1,49 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-        <ul class="navbar-nav mr-auto">
-            <h1>BoolBnB</h1>
-        </ul>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            
+        <div class="row width_100">
+            <div class="col-sm">
+                <ul class="navbar-nav mr-auto">
+                    <h1>BoolBnB</h1>
+                </ul>
+            </div>
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @if (Route::has('login'))
-                    <div class="top-right links">
-                        @auth
-                            <a href="{{ url('/admin/apartments') }}">Home</a>
-                        @else
-                            <a href="{{ route('login') }}">Login</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Register</a>
-                            @endif
-                        @endauth
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+           
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="col-sm">
+                        <!-- Left Side Of Navbar -->
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
                     </div>
-                @endif
-            </ul>
+
+                    <div class="col-sm">
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @if (Route::has('login'))
+                                <div class="top-right links">
+                                    @auth
+                                        <a href="{{ url('/admin/apartments') }}">Home</a>
+                                    @else
+                                        <a href="{{ route('login') }}">Login</a>
+            
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}">Register</a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                        </ul>
+                    </div>
+                </div>  
+            
+               
         </div>
+        
     </div>
 </nav>
