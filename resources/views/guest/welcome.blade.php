faf721ba01c740c66882ef6c7167df89414de65c
Flavia <flaviamatta90@gmail.com>
2020-11-27 16:00:58 +0100

    apartment fix

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -4,13 +4,14 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container_apartments">
         <div class="apartments_premium">
             @foreach ($apartments_premium as $apartment_premium)
                 <div class="apartment_premium_box">
                     <h3>{{ $apartment_premium['title'] }}</h3>
                     <img src="{{$apartment_premium['cover']}}" alt="cover">
                     <p>{{ $apartment_premium['description'] }}</p>
+                    {{-- <a href="{{ route('guest.show') }}"> --}}<button class="badge badge-info">Info</button></a>
                 </div>
             @endforeach
         </div>
@@ -20,6 +21,7 @@
                     <h3>{{ $apartment_free['title'] }}</h3>
                     <img src="{{$apartment_free['cover']}}" alt="cover">
                     <p>{{ $apartment_free['description'] }}</p>
+                    <button class="badge badge-info">Info</button></a>
                 </div>
             @endforeach
         </div>
