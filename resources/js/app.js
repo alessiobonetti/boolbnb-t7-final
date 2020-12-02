3c44975af70497be96081eb7413a02dbc464ccad
Flavia <flaviamatta90@gmail.com>
2020-12-02 12:40:47 +0100

    maps+images guestControl

--- resources/js/app.js
+++ resources/js/app.js
@@ -104,30 +104,3 @@ require('startbootstrap-sb-admin-2/js/demo/chart-pie-demo.js');
 //         $(".row_search").append(html);
 //     }
 // }
-
-function initMap() {
-    const myLatlng = { lat: -25.363, lng: 131.044 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatlng,
-    });
-    // Create the initial InfoWindow.
-    let infoWindow = new google.maps.InfoWindow({
-      content: "Click the map to get Lat/Lng!",
-      position: myLatlng,
-    });
-    infoWindow.open(map);
-    // Configure the click listener.
-    map.addListener("click", (mapsMouseEvent) => {
-      // Close the current InfoWindow.
-      infoWindow.close();
-      // Create a new InfoWindow.
-      infoWindow = new google.maps.InfoWindow({
-        position: mapsMouseEvent.latLng,
-      });
-      infoWindow.setContent(
-        JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-      );
-      infoWindow.open(map);
-    });
-  }
