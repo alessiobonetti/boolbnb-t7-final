5ba8a509da9dd883f87c12778c2ecc92968b1bfe
Alessio Bonetti <alesine87@gmail.com>
2020-12-03 18:35:54 +0100

    jumbotron
    aggiunto sfondo con opacity per il titolo

--- public/js/app.js
+++ public/js/app.js
@@ -59195,8 +59195,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Luca\Desktop\Classe17\boolbnb-t7-final\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Luca\Desktop\Classe17\boolbnb-t7-final\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\boolbnb-t7-final\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\boolbnb-t7-final\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
