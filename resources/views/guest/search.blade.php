fb8ec9f6b1b8c4dbd2ed67185fc22cc14d94a8c2
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-12-03 15:50:38 +0100

    ok fix.welcome

--- resources/views/guest/search.blade.php
+++ resources/views/guest/search.blade.php
@@ -1,38 +1,37 @@
 @include('partials.headerSearch')
 @extends('layouts.main')
 @section('content')
-<div class="container">
 
-    <div id="apartments_premium">
+    <div id="apartments_premium" class="container">
     </div>
-</div>
+
 
 
 {{-- Qui sono dentro a search --}}
     {{-- handlebars template --}}
-    <script id="apartments_template" type="text/x-handlebars-template">
-        <div class="container_apartments">
-            <div class="card-deck col-3">
-                <div class="card change_class premium_class">
-                    <img class="card-img-top" src="@{{cover}}">
+<script id="apartments_template" type="text/x-handlebars-template">
+    <div class="container_apartments mb-3">
+        <div class="card-deck col">
+            <div class="card change_class premium_class">
+                <img class="card-img-top" src="@{{cover}}">
 
-                    <div class="card-body">
-                        <h5 class="card-title">@{{title}}</h5>
-                        <p class="card-text">@{{description}}</p>
-                    </div>
+                <div class="card-body">
+                    <h5 class="card-title">@{{title}}</h5>
+                    <p class="card-text">@{{description}}</p>
+                </div>
 
-                    <div class="card-footer span2">
-                        <a href="apartment/@{{apartmentId}}"><button class="btn-block badge badge-info">Info</button></a>
-                    </div>
+                <div class="card-footer span2">
+                    <a href="apartment/@{{apartmentId}}"><button class="btn-block badge badge-info">Info</button></a>
                 </div>
             </div>
         </div>
-      </script>
+    </div>
+</script>
 
 
-    {{-- Inserito cdn jquery - modificare librerie --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+{{-- Inserito cdn jquery - modificare librerie --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
 
