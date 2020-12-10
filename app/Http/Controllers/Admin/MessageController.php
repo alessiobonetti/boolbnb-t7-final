e684e1db0f40ec8d7f5d742c7584d0826cacbd63
Alessio Bonetti <alesine87@gmail.com>
2020-12-10 13:00:37 +0100

    message utente fix

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -6,6 +6,7 @@ use App\Apartment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Message;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
@@ -13,12 +14,13 @@ class MessageController extends Controller
     public function index()
     {
         $id = Auth::id();
+        $user = User::where('id' , $id)->select('email')->get();
         $apartments = Apartment::has('messages')
             ->where('user_id', $id)
             ->orderBy('created_at', 'asc')
             ->get()
             ->toArray();
         $messages = Message::whereIn('apartment_id', [$apartments])->get();
-        return view('admin.message', compact('messages'));
+        return view('admin.message', compact('messages','user'));
     }
 }
