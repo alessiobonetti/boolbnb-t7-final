90b900c112ba63943d318ee9cdd6d8acf2d4febc
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-12-11 09:56:46 +0100

    chart fix

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -4,19 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Apartment;
 use App\Http\Controllers\Controller;
-use App\Charts\SampleChart;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class ChartController extends Controller
 {
     public function index()
     {
-        $apartments=Apartment::all();
+        $id = Auth::id();
+        $apartments = Apartment::all()->where('user_id', $id);
 
-        $chart = new SampleChart;
 
-        $message = DB::table('apartments')->where('active', 1)->whereYear('created_at')->sum('message');
-        $view = DB::table('apartments')->where('active', 1)->whereYear('created_at')->sum('view');
 
         return view('admin.chart', compact('apartments'));
     }
--- public/js/app.js
+++ public/js/app.js
@@ -37726,8 +37726,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\boolbnb-t7-final\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\alesi\Desktop\Boolean\Esercizi\boolbnb-t7-final\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/carloboolean/Desktop/boolean/test/boolbnb/boolbnb-t7-final/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/carloboolean/Desktop/boolean/test/boolbnb/boolbnb-t7-final/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
